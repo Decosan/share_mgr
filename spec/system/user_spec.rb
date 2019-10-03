c57a5f79f1e606eefb1f9501ac793a258efa1ead
Decosan <junx0401@gmail.com>
2019-10-03 18:29:05 +0900

    Add:Rspec 01

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -2,29 +2,45 @@ require 'rails_helper'
 
 RSpec.describe "User", type: :system do
   
-  describe 'User一覧' do
+  describe 'User登録' do
     context '正常系' do
-      fit 'サインアップ後は個人情報登録画面へ遷移し、登録後はUser詳細画面へ遷移すること' do
+      it 'サインアップ後は個人情報登録画面へ遷移し、登録後はUser詳細画面へ遷移すること' do
         # TODO: ローカル変数ではなく let を使用してください
-        visit sign_in_path
-        @user1 = User.new(name:'test',email:'test@123.com',password:'123456')
-        click_link 'Sign up'
+        expect {
+      visit new_user_registration_path
+      fill_in "ID名", with: "TEST"
+      fill_in "メールアドレス", with: "test@123.com"
+      fill_in "パスワード", with: "123456"
+      fill_in "確認用パスワード", with: "123456"
+      click_on 'Sign up'
+      }.to change(User, :count).by(1)
         expect(current_path).to eq new_personnel_path
-        FactoryBot.create(:personnel, last_name: "tester", first_name: 'suzuki', birth_date: '1999/12/31', gender: 0, room: 0, in_date: '2019/10/10', out_plan_date: '2020/10/10', user_id: @user.id)
-        click_link '登録'
-        expect(current_path).to eq user_path(@user.id)
+        fill_in "姓（せい）", with: "ざっく"
+        fill_in "名（めい）", with: "じょんそん"
+        select '男性', from: '性別'
+        select "101b", from: '部屋番号'
+        click_on '登録'
+        expect(page).to have_selector 'h1', text: 'User Profile'
       end
 
-      it 'Project詳細から一覧ページにアクセスした場合、Taskが表示されること' do
-        # FIXME: テストが失敗するので修正してください
-        project = FactoryBot.create(:project)
-        task = FactoryBot.create(:task, project_id: project.id)
-        visit project_path(project)
-        click_link 'View Todos'
-        switch_to_window(windows.last)
-        expect(page).to have_content task.title
-        expect(Task.count).to eq 1
-        expect(current_path).to eq project_tasks_path(project)
+      fit '個人情報の登録を完了していない場合、他のページへは遷移できない' do
+          expect {
+        visit new_user_registration_path
+        fill_in "ID名", with: "TEST"
+        fill_in "メールアドレス", with: "test@123.com"
+        fill_in "パスワード", with: "123456"
+        fill_in "確認用パスワード", with: "123456"
+        click_on 'Sign up'
+        }.to change(User, :count).by(1)
+          expect(current_path).to eq new_personnel_path
+          fill_in "姓（せい）", with: "ざっく"
+          fill_in "名（めい）", with: "じょんそん"
+          select '男性', from: '性別'
+          select "101b", from: '部屋番号'
+          click_on 'トップページ'
+          expect(current_path).to eq new_personnel_path
+          click_on 'マイページ'
+          expect(current_path).to eq new_personnel_path
       end
     end
   end
