a7f5c40e07a19dfbfe4a3409258fe28c1029a7fa
Decosan <junx0401@gmail.com>
2019-10-20 14:35:11 +0900

    fixed_front_pages

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_action :set_comment, only:[:destroy]
   before_action :correct_user, only:[:destroy]
   
   def create
@@ -15,13 +16,16 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     flash[:danger] ='コメント削除しました'
     redirect_back(fallback_location: root_path)
   end
 
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
   
   def comment_params
     params.require(:comment).permit(:event_id, :content)
