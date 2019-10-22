73487045b1a2d2016d670f4954251f2e2f0cb8b4
Decosan <junx0401@gmail.com>
2019-10-22 11:02:09 +0900

    バリデーションの追加：日付のマイナスNG events,assignments / 今日の日付でFix card_payments

--- db/migrate/20190929015927_create_card_payments.rb
+++ db/migrate/20190929015927_create_card_payments.rb
@@ -1,7 +1,7 @@
 class CreateCardPayments < ActiveRecord::Migration[5.2]
   def change
     create_table :card_payments do |t|
-      t.date :month
+      t.date :month, default: -> { "now()" }
       t.integer :amount, default: 0, null: false
       t.integer :confirm, default: 0, null: false
       t.references :user, foreign_key: true
